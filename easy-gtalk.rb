14f745700ebe571fb0049143ca6b92909dba9709
DanielVartanov <daniel.vartanov@gmail.com>
2010-06-20 21:12:12 +0600

    Initial commit

---
+++ easy-gtalk.rb
@@ -0,0 +1,71 @@
+require 'openssl'
+
+require "xmpp4r"
+require 'xmpp4r/roster'
+
+module Jabber
+  module JIDExtensions
+    def to_email
+      "#{self.node}@#{self.domain}"
+    end
+  end
+
+  JID.__send__ :include, JIDExtensions
+end
+
+module GTalk
+  class Bot
+    include Jabber
+    attr_reader :email, :password
+    attr_reader :jabber_client, :jid
+
+    def initialize(account_data)
+      @email = account_data[:email]
+      @password = account_data[:password]
+
+      @jid = JID::new(self.email)
+      @jabber_client = Client.new(self.jid)
+    end
+
+    def get_online
+      self.jabber_client.connect
+      self.jabber_client.auth(self.password)
+      self.jabber_client.send(Presence.new.set_type(:available))
+    end
+
+    def invite(invitee)
+      subscription_request = Presence.new.set_type(:subscribe).set_to(JID::new(invitee))
+      self.jabber_client.send(subscription_request)
+    end
+
+    def accept_invitation(inviter)
+      inviter = JID::new(inviter)
+      self.roster.accept_subscription(inviter)
+      invite(JID::new(inviter))
+    end
+
+    def message(to, text)
+      message = Message::new(JID::new(to), text)
+      message.type = :chat
+      self.jabber_client.send(message)
+    end
+
+    def on_invitation(&block)
+      self.roster.add_subscription_request_callback do |_, presence|
+        block.call(presence.from.to_email)
+      end
+    end
+
+    def on_message(&block)
+      self.jabber_client.add_message_callback do |message|
+        block.call(message.from.to_email, message.body)
+      end
+    end
+
+  protected
+
+    def roster
+      @roster ||= Roster::Helper.new(self.jabber_client)
+    end
+  end
+end
---
+++ sandbox.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+
+require 'easy-gtalk'
+
+bot = GTalk::Bot.new(:email => 'dasher.bot.2@gmail.com', :password => 'botniibet')
+bot.get_online
+
+bot.on_invitation do |inviter|
+  puts "I have been invited by #{inviter}. Yay!"
+
+  bot.accept_invitation(inviter)
+  bot.message(inviter, "Hello there! Thanks for invitation!")
+end
+
+bot.on_message do |from, text|
+  puts "I got message from #{from}: '#{text}'"
+
+  bot.message from, "You've told me something!"
+end
+
+Thread.stop
+
+#get_online :invisible => true
+#bot.set_status :away
+#bot.set_status :available
+
+#GTalk::Account('aldor.kg@gmail.com').available?
+#GTalk::Account('aldor.kg@gmail.com').online?
+#
+#GTalk::Account('aldor.kg@gmail.com').in_contact_list?
+
+#bot.contact_list # => [GTalk::Account.new, GTalk::Account.new]
